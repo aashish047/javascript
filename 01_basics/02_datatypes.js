c350a65cee0c6f96f72ca9dd8ac44900322903f1
aashish047 <157306816+aashish047@users.noreply.github.com>
2024-01-21 18:07:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" ;//trat all JS code as newer version ( new standard of JS )
+
+//alert( 3 + 3 ) // we are ussing node JS not browser
+
+console.log(3+3) 
+console.log("aashish")
+
+//datatypes
+ let name = " aashish "
+ let age  = 19
+ let isLoggedin = false
+
+ //number => range 2^53
+ //bigint => for big no.
+ //string => " "
+ //boolean =>true/false
+ //null(datatype as well a value / standalone value)
+ //undefined => value is not assigned
+ //symbol => unqiue
+
+
+ //object
+
+
+
+ console.log(typeof"aaashish");
+ console.log(typeof age);
+ console.log(typeof null);  // null is a object
